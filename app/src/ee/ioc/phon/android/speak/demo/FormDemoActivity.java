82039acfc6d3d70b685d10edfcb8f5fcb645791b
Kaarel Kaljurand <kaljurand@gmail.com>
2016-07-05 00:37:09 +0200

    Minor updates

--- app/src/ee/ioc/phon/android/speak/demo/FormDemoActivity.java
+++ app/src/ee/ioc/phon/android/speak/demo/FormDemoActivity.java
@@ -1,18 +1,44 @@
 package ee.ioc.phon.android.speak.demo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import ee.ioc.phon.android.speak.R;
 
 public class FormDemoActivity extends Activity {
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /**
+         * Show a toast from the web page
+         */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView webview = new WebView(this);
         setContentView(webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.addJavascriptInterface(new WebAppInterface(this), "Android");
         webview.loadUrl(getString(R.string.fileFormDemo));
     }
 
