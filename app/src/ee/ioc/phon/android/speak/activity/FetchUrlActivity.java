bcc5b8d17162fa39d4ccea3ab632aea700b18fbd
Kaarel Kaljurand <kaljurand@gmail.com>
2017-01-28 13:21:50 +0100

    Move some static HTTP methods to speechutils

--- app/src/ee/ioc/phon/android/speak/activity/FetchUrlActivity.java
+++ app/src/ee/ioc/phon/android/speak/activity/FetchUrlActivity.java
@@ -25,14 +25,8 @@ import android.support.annotation.NonNull;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import ee.ioc.phon.android.speak.Log;
+import ee.ioc.phon.android.speechutils.utils.HttpUtils;
 import ee.ioc.phon.android.speechutils.utils.IntentUtils;
 
 /**
@@ -73,64 +67,6 @@ public class FetchUrlActivity extends Activity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
-    private static String fetchUrl(String myurl, String method, String body) throws IOException {
-        byte[] outputInBytes = null;
-
-        if (body != null) {
-            outputInBytes = body.getBytes("UTF-8");
-        }
-
-        InputStream is = null;
-
-        try {
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(5000 /* milliseconds */);
-            conn.setConnectTimeout(5000 /* milliseconds */);
-            conn.setRequestMethod(method);
-            conn.setDoInput(true);
-
-            if (outputInBytes == null) {
-                conn.connect();
-            } else {
-                conn.setDoOutput(true);
-                OutputStream os = conn.getOutputStream();
-                os.write(outputInBytes);
-                os.close();
-            }
-
-            //int response = conn.getResponseCode();
-            is = conn.getInputStream();
-            return inputStreamToString(is, 1024);
-        } finally {
-            closeQuietly(is);
-        }
-    }
-
-    private static void closeQuietly(InputStream is) {
-        try {
-            if (is != null) {
-                is.close();
-            }
-        } catch (Exception e) {
-            Log.i(e.getMessage());
-        }
-    }
-
-    private static String inputStreamToString(final InputStream is, final int bufferSize) throws IOException {
-        final char[] buffer = new char[bufferSize];
-        final StringBuilder out = new StringBuilder();
-        Reader in = new InputStreamReader(is, "UTF-8");
-        while (true) {
-            int rsz = in.read(buffer, 0, buffer.length);
-            if (rsz < 0) {
-                break;
-            }
-            out.append(buffer, 0, rsz);
-        }
-        return out.toString();
-    }
-
     private class HttpTask extends AsyncTask<String, Void, String> {
 
         private final Bundle mBundle;
@@ -146,7 +82,7 @@ public class FetchUrlActivity extends Activity {
         @Override
         protected String doInBackground(String... urls) {
             try {
-                return fetchUrl(urls[0], mHttpMethod, mHttpBody);
+                return HttpUtils.fetchUrl(urls[0], mHttpMethod, mHttpBody);
             } catch (IOException e) {
                 return "Unable to retrieve " + urls[0] + " " + e.getLocalizedMessage();
             }
