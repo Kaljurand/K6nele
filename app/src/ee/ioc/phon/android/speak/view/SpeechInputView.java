Kaljurand/K6nele

commit 33f469687fdd814cdb585be60ab74a811bbac2aa

Update androidasync + minor changes

build.gradle
@@ -6,7 +6,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.0.0-alpha8'
+        classpath 'com.android.tools.build:gradle:3.0.0-alpha9'
     }
 }
 

app/build.gradle
@@ -7,7 +7,7 @@ dependencies {
     compile 'com.googlecode.json-simple:json-simple:1.1'
     // TODO: This did not work
     //compile 'com.googlecode.json-simple:json-simple:1.1.1'
-    compile 'com.koushikdutta.async:androidasync:2.1.8'
+    compile 'com.koushikdutta.async:androidasync:2.2.1'
     compile 'com.android.support:support-v13:' + rootProject.supportLibVersion
     // Enable this when using the support library for the vector drawables
     //compile 'com.android.support:appcompat-v7:23.2.1'

docs/adb-pref.py
@@ -59,6 +59,7 @@ def create_adb(pref):
         TODO: incomplete + should escape for adb as well (e.g. the comma)
         """
         text = re.sub('\n', '\\\n', text)
+        text = re.sub('\t', '\\\t', text)
         text = re.sub(' ', '\\ ', text)
         return text
 

docs/prefs_developer.yml
@@ -40,7 +40,7 @@
   val: false
 
 - key: keyAutoStopAfterTime
-  val: "10"
+  val: '10'
 
 #
 # WebSocket service

commit a94b8099c58b7289a34d06a179e0cfe6ec12d746

Fix regression: long press on combo selector fails
Due to change in 33f469687 ComboSelectorActivity could not be opened anymore
because it is not exported.

app/src/ee/ioc/phon/android/speak/view/SpeechInputView.java
@@ -29,7 +29,6 @@ import ee.ioc.phon.android.speak.activity.ComboSelectorActivity;
 import ee.ioc.phon.android.speak.model.CallerInfo;
 import ee.ioc.phon.android.speak.model.Combo;
 import ee.ioc.phon.android.speechutils.Extras;
-import ee.ioc.phon.android.speechutils.utils.IntentUtils;
 import ee.ioc.phon.android.speechutils.utils.PreferenceUtils;
 import ee.ioc.phon.android.speechutils.view.MicButton;
 
@@ -260,7 +259,7 @@ public class SpeechInputView extends LinearLayout {
                     Intent intent = new Intent(context, ComboSelectorActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     intent.putExtra("key", context.getString(key));
-                    IntentUtils.startActivityIfAvailable(context, intent);
+                    context.startActivity(intent);
                     return true;
                 }
             });

build.gradle
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.0.0-alpha9'
+        classpath 'com.android.tools.build:gradle:3.0.0-beta2'
     }
 }
 